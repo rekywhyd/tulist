d573dcdc0d50555e46f116d01216be5194f072a4
rekywhyd <rekywahyudi00@gmail.com>
2025-11-12 01:09:23 +0700

    menambah favicon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Tulist</title>
-        
+        <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+
         @vite(['resources/css/app.css', 'resources/js/app.js'])    
     </head>
     
