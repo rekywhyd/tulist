f417e1ac61d077441d8a85da6acb7a71aac08d54
rekywhyd <rekywahyudi00@gmail.com>
2025-11-19 18:52:23 +0700

    berhasil verify email

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,29 +1,33 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+    <div class="flex flex-col items-center font-serif">
+        <h1 class="mt-2 mb-4 text-4xl font-bold text-gray-900">Verify your email</h1>
+        <div class="text-xs font-semibold text-gray-500 font-poppins">
+            {{ __('Please verify your email via the link we just sent. If you didn\'t receive it, we can resend it.') }}
+        </div>
     </div>
 
     @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 text-sm font-medium text-green-600">
+        <div class="mt-4 text-xs font-semibold text-green-600">
             {{ __('A new verification link has been sent to the email address you provided during registration.') }}
         </div>
     @endif
 
-    <div class="flex items-center justify-between mt-4">
+    <div class="flex items-center justify-between">
         <form method="POST" action="{{ route('verification.send') }}">
             @csrf
 
-            <div>
-                <x-primary-button>
-                    {{ __('Resend Verification Email') }}
-                </x-primary-button>
-            </div>
+            
+                <div class="flex justify-center mt-6 mb-5 transition-transform duration-200 font-poppins hover:hover:scale-105">
+            <x-primary-button class="flex justify-center px-[30px] py-[16px] text-xs font-medium text-white bg-[#163769] border border-transparent shadow-sm rounded-2xl hover:bg-[#132C51] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-700">
+                {{ __('Resend Verification Email') }}
+            </x-primary-button>
+        </div>
         </form>
 
         <form method="POST" action="{{ route('logout') }}">
             @csrf
 
-            <button type="submit" class="text-sm text-gray-600 underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            <button type="submit" class="mt-4 font-semibold text-[#6AA6FF] hover:underline font-poppins text-sm underline rounded-md hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                 {{ __('Log Out') }}
             </button>
         </form>
--- routes/web.php
+++ routes/web.php
@@ -47,7 +47,7 @@ Route::get('/help', [PageController::class, 'help'])->name('help')->middleware('
 Route::get('/privacy', [PageController::class, 'privacy'])->name('privacy')->middleware('auth');
 Route::get('/notifications', [PageController::class, 'notifications'])->name('notifications')->middleware('auth');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'verified')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
